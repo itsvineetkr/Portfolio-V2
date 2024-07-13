9b07aa479f3f4d346cebe7617307f887ba151600
itsvineetkr <itsvineetkr@gmail.com>
2024-07-13 23:44:37 +0530

    new_scroll_type

--- home/urls.py
+++ home/urls.py
@@ -3,9 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='homepage'),
-    path('about/', views.about, name='about'),
     path('resume/', views.resume, name='resume'),
-    path('projects/', views.projects, name='projects'),
-    path('contact/', views.contact, name='contact'),
-
 ]
--- home/views.py
+++ home/views.py
@@ -3,18 +3,6 @@ from .models import Messages
 
 
 def index(request):
-    return render(request, 'index.html')
-
-def about(request):
-    return render(request, 'about.html')
-
-def projects(request):
-    return render(request, 'projects.html')
-
-def resume(request):
-    return render(request, 'resume.html')
-
-def contact(request):
     if request.method == "POST":
         name = request.POST.get("name")
         email = request.POST.get("email")
@@ -24,4 +12,7 @@ def contact(request):
             messageData = Messages(name=name, email=email, message=message)
             messageData.save()
 
-    return render(request, 'contact.html')
+    return render(request, 'index.html')
+
+def resume(request):
+    return render(request, 'resume.html')

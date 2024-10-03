0be0918ab1859e0da07d2b98d9f53dabf74fcd40
itsvineetkr <itsvineetkr@gmail.com>
2024-10-03 15:10:12 +0530

    New project and few changes

--- home/urls.py
+++ home/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 from . import views
 
 urlpatterns = [
     path('', views.index, name='homepage'),
     path('resume/', views.resume, name='resume'),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
